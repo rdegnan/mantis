a23b12b3a7846dbcea399189eb494ce4fd8a772e
Ryland Degnan <rdegnan@twitter.com>
2020-11-17 15:18:05 -0800

    Updates for building local docker containers

--- mantis-examples/mantis-examples-mantis-publish-sample/buildDockerImage.sh
+++ mantis-examples/mantis-examples-mantis-publish-sample/buildDockerImage.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # build the mantis-server-worker fat jar
-../gradlew clean build shadowDistZip
-unzip build/distributions/mantis-examples-mantis-publish-sample-shadow-0.1.0-SNAPSHOT.zip -d build/distributions 
+../../gradlew clean build shadowDistZip
+unzip build/distributions/mantis-examples-mantis-publish-sample-shadow-1.4.0-SNAPSHOT.zip -d build/distributions
 # build the Docker image that packages the mantis-server-worker along with a running mesos-slave
 docker build -t dev/mantispublish .
 
