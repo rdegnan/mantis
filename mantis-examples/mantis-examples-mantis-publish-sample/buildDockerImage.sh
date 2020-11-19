9c9162152d44d507932f0a1874059ef2c3ff9af1
Ryland Degnan <rdegnan@twitter.com>
2020-11-19 14:30:56 -0800

    Build fixes

--- mantis-examples/mantis-examples-mantis-publish-sample/buildDockerImage.sh
+++ mantis-examples/mantis-examples-mantis-publish-sample/buildDockerImage.sh
@@ -2,7 +2,7 @@
 
 # build the mantis-server-worker fat jar
 ../../gradlew clean build shadowDistZip
-unzip build/distributions/mantis-examples-mantis-publish-sample-shadow-1.4.0-SNAPSHOT.zip -d build/distributions
+unzip build/distributions/mantis-examples-mantis-publish-sample-shadow-2.0.0-SNAPSHOT.zip -d build/distributions
 # build the Docker image that packages the mantis-server-worker along with a running mesos-slave
 docker build -t dev/mantispublish .
 
